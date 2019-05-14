fb4580f1c953a36d385b9427408269c419efe345
Michal Kalinowski <subaru.kali@gmail.com>
2019-05-14 16:00:42 +0200

    new class created

---
+++ src/com/codecool/car_race/vehicles/Vehicle.java
@@ -0,0 +1,19 @@
+package com.codecool.car_race.vehicles;
+
+import com.codecool.car_race.Race;
+
+public class Vehicle {
+    int distanceTraveled;
+    String name;
+
+
+    // setup the actual speed used for the current lap
+    public void prepareForLap(Race race) {
+
+    }
+
+    // The vehicle travels for an hour. It increases the distance traveled. Call this from the Race::simulateRace() only!
+    public void moveForAnHour() {
+
+    }
+}
