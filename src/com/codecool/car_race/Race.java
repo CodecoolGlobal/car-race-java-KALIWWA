c1474bfa8ef4a6d937570bdb911a35aad5c8fd11
Michal Kalinowski <subaru.kali@gmail.com>
2019-05-14 11:41:44 +0200

    simulateRace & printRaceResults added

--- src/com/codecool/car_race/Race.java
+++ src/com/codecool/car_race/Race.java
@@ -2,4 +2,11 @@ package com.codecool.car_race;
 
 public class Race {
 
+    public void simulateRace() {
+        System.out.println("Race");
+    }
+
+    public void printRaceResults() {
+        System.out.println("Race Results");
+    }
 }
